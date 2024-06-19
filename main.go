abf877dbacbb8d85713db904b7b97a171e0de71c
Sajan Toor <sajantoor98@gmail.com>
2024-06-18 21:26:53 -0700

    Create server

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(requestHandler),
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+	}
+
+	fmt.Println("Server running on port 3000")
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
 }
